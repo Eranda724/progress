70534667bdc58050a28fb9f0515f972215cb4e5a
Eranda724 <erandajayasinghe724@gmail.com>
2025-03-15 13:12:20 +0530

    fin

--- src/main/java/com/example/Book/controller/HelloController.java
+++ src/main/java/com/example/Book/controller/HelloController.java
@@ -1,15 +1,16 @@
 package com.example.Book.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String greet(HttpServletRequest request) {
-        return "Welcome to Telusko " + request.getSession().getAttribute("username");
+        return "Welcome.  " + request.getSession().getAttribute("username");
     }
 
 }
